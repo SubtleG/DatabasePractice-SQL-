b79d91b1daef34c9d2f2017c14e4751b9d07b9b3
SubtleG <4bryceh@gmail.com>
2017-11-28 11:58:59 -0800

    Inital Commit. Hello World c++ Program

---
+++ Database-SQL/src/Database-SQL.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Database-SQL.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//My SQL stuff
+	cout << "Program ending, Have a nice day!" << endl; // prints Program ending, Have a nice day!
+	return 0;
+}
